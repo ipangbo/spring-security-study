6cff2fb524ec531e11122ad74ef366d85354409b
ipangbo <122402331@qq.com>
2022-12-09 01:07:01 +0800

    form based login

--- src/main/java/cn/ipangbo/study/security/ApplicationSecurityConfig.java
+++ src/main/java/cn/ipangbo/study/security/ApplicationSecurityConfig.java
@@ -3,15 +3,21 @@ package cn.ipangbo.study.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RegexRequestMatcher;
+
+import java.util.concurrent.TimeUnit;
 
 import static cn.ipangbo.study.security.ApplicationUserRole.*;
 
@@ -45,7 +51,27 @@ public class ApplicationSecurityConfig {
                 .authenticated()
 
                 .and()
-                .httpBasic();
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .defaultSuccessUrl("/courses", true)
+                .usernameParameter("username")
+                .passwordParameter("password")
+
+                .and()
+                .rememberMe()
+                .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21)) // defaults to 2 wks
+                .key("asdfghjkl;'")
+                .rememberMeParameter("remember-me")
+
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+                .clearAuthentication(true)
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID", "remember-me")
+                .logoutSuccessUrl("/login");
 //                .and()
 //                .authenticationProvider();
         return httpSecurity.build();
@@ -53,7 +79,7 @@ public class ApplicationSecurityConfig {
 
 //    @Bean
 //    public WebSecurityCustomizer webSecurityCustomizer() {
-//        return web -> web.ignoring().antMatchers("/login");
+//        return web -> web.ignoring().requestMatchers("/login");
 //    }
 
     @Bean
