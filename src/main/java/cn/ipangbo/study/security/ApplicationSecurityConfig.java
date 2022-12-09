0f4b37913d56b812c7d91d5568958ea69a65c12b
ipangbo <122402331@qq.com>
2022-12-09 12:55:06 +0800

    form based login

--- src/main/java/cn/ipangbo/study/security/ApplicationSecurityConfig.java
+++ src/main/java/cn/ipangbo/study/security/ApplicationSecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -74,13 +73,6 @@ public class ApplicationSecurityConfig {
 
         return httpSecurity.build();
     }
-
-//    @Bean
-//    public WebSecurityCustomizer webSecurityCustomizer() {
-//        return web -> web.ignoring().requestMatchers("/login");
-//    }
-
-
 
     @Bean
     public DaoAuthenticationProvider daoAuthenticationProvider() {
